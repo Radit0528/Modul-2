b3d1f77560841416b78e66a230a1e843f52c694c
Radit0528 <91658360+Radit0528@users.noreply.github.com>
2024-03-25 20:12:55 +0700

    Update unguided2.cpp

--- unguided2.cpp
+++ unguided2.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main()
 {
-    int x_2311102103, y_2311102103, z_2311102103;
+    int x_2311102013, y_2311102013, z_2311102013;
 
     cout << "Masukkan ukuran dimensi x: ";
-    cin >> x_2311102103;
+    cin >> x_2311102013;
     cout << "Masukkan ukuran dimensi y: ";
-    cin >> y_2311102103;
+    cin >> y_2311102013;
     cout << "Masukkan ukuran dimensi z: ";
-    cin >> z_2311102103;
+    cin >> z_2311102013;
 
-    int arr[x_2311102103][y_2311102103][z_2311102103];
+    int arr[x_2311102013][y_2311102013][z_2311102013];
 
     cout << "Masukkan nilai untuk array 3 dimensi:" << endl;
-    for (int x = 0; x < x_2311102103; ++x)
+    for (int x = 0; x < x_2311102013; ++x)
     {
-        for (int y = 0; y < y_2311102103; ++y)
+        for (int y = 0; y < y_2311102013; ++y)
         {
-            for (int z = 0; z < z_2311102103; ++z)
+            for (int z = 0; z < z_2311102013; ++z)
             {
                 cout << "Masukkan nilai array[" << x << "][" << y << "][" << z << "]: ";
                 cin >> arr[x][y][z];
@@ -29,11 +29,11 @@ int main()
 
     // Tampilan array
     cout << "\nTampilan Array:\n";
-    for (int x = 0; x < x_2311102103; x++)
+    for (int x = 0; x < x_2311102013; x++)
     {
-        for (int y = 0; y < y_2311102103; y++)
+        for (int y = 0; y < y_2311102013; y++)
         {
-            for (int z = 0; z < z_2311102103; z++)
+            for (int z = 0; z < z_2311102013; z++)
             {
                 cout << arr[x][y][z] << " ";
             }
